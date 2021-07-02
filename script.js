9cf192f921a82787154fd7a19a2df4671ccea0fc
barto14753 <wlodarski_bartosz@o2.pl>
2021-07-02 14:00:12 +0200

    add color picker and navbar

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
+$('#color-picker').spectrum({
+    type: "color"
+});
+
+
+//$("#picker").spectrum("get");
+
 var canvas = document.getElementById("canvas");
-var menu = document.getElementById("menu");
+var palette = document.getElementById("palette");
 var ctx = canvas.getContext("2d");
 
-let size = 10;
+let mousePressed = false;
+let drawDelay = 300;
+let size = 5.0;
 
+function getPos(event) {
+    let rect = canvas.getBoundingClientRect(),
+        scaleX = canvas.width / rect.width,
+        scaleY = canvas.height / rect.height;
 
-function draw(event)
-{
-    let x = (event.pageX);
-    let y = (event.pageY);
-    console.log(x,y);
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
-    ctx.stroke;
+    return {
+        x: (event.clientX - rect.left) * scaleX,
+        y: (event.clientY - rect.top) * scaleY
+    }
+}
+
+function draw(event) {
+    if (mousePressed) {
+        let pos = getPos(event);
+        console.log(pos.x, pos.y);
+        ctx.beginPath();
+        ctx.arc(pos.x, pos.y, size, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke;
+        //setTimeout(draw, drawDelay);
+    }
+
 
 }
 
-document.getElementById("canvas").addEventListener("click", function(event)
-{
+document.getElementById("canvas").addEventListener("mousedown", function(event) {
+    console.log("Down");
+    mousePressed = true;
     draw(event);
 });
+
+document.getElementById("canvas").addEventListener("mouseup", function(event) {
+    console.log("Up");
+    mousePressed = false;
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     let mousePosX = 0,
         mousePosY = 0,
@@ -31,16 +60,13 @@ document.addEventListener('DOMContentLoaded', () => {
         mousePosX = e.pageX;
         mousePosY = e.pageY;
 
-        let bounds = menu.getBoundingClientRect();
+        let bounds = palette.getBoundingClientRect();
         if (mousePosX > bounds.left &&
             mousePosX < bounds.right &&
             mousePosY < bounds.bottom &&
-            mousePosY > bounds.top)
-        {
+            mousePosY > bounds.top) {
             mouseCircle.style.visibility = 'hidden';
-        }
-        else
-        {
+        } else {
             mouseCircle.style.visibility = 'visible';
         }
     }
@@ -54,12 +80,10 @@ document.addEventListener('DOMContentLoaded', () => {
         requestAnimationFrame(delayMouseFollow);
 
         revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-        revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
+        revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
 
         mouseCircle.style.top = revisedMousePosY + 'px';
         mouseCircle.style.left = revisedMousePosX + 'px';
     }
     delayMouseFollow();
 });
-
-
